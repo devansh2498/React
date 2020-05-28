7945430f8f4fbbaaf5d7d9c3f7e0a097d285e4aa
Devansh <devansh2498@gmail.com>
2020-05-28 11:30:05 +0530

    Components-1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
